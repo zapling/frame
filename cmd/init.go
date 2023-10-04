a2a2fe7f5f6ee8fdc01aea8f9526d8b04edd7b1b
Andreas Palm <andreaspalm98@gmail.com>
2023-10-04 23:17:56 +0200

    Init command to create project directory if not exists

--- cmd/init.go
+++ cmd/init.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -11,9 +12,25 @@ func init() {
 }
 
 var initCmd = &cobra.Command{
-	Use:   "init",
+	Use:   "init [project-name]",
 	Short: "Initialize a new project",
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello from init")
+		projectName := args[0]
+
+		if _, err := os.Stat(projectName); !os.IsNotExist(err) {
+			fmt.Println("Directory already exists!")
+			os.Exit(1)
+		}
+
+		if err := os.Mkdir(projectName, 0755); err != nil {
+			fmt.Printf("Failed to create project directory: %v", err)
+			os.Exit(1)
+		}
+
+		if err := os.Chdir(projectName); err != nil {
+			fmt.Printf("Failed to change working directory to %s", projectName)
+			os.Exit(1)
+		}
 	},
 }
